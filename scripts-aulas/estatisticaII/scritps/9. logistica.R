d9c80a84ee21b4234df01d5a03c4de05c51b4d59
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-08 13:02:35 -0300

    exercícios

--- scripts-aulas/estatisticaII/scritps/9. logistica.R
+++ scripts-aulas/estatisticaII/scritps/9. logistica.R
@@ -20,3 +20,17 @@ prevereleicao$RESULT = predict(modelo,newdata=prevereleicao,type="response")
 prevereleicao$RESULT
 #fix(prevereleicao)
 predict(modelo, as.data.frame(DESPESAS = 1000))
+
+#exercicios
+x=c(15,18,20,25,30,44)
+y=c(240,255,270,283,300,310)
+cor(x,y)
+
+women
+modeloW = lm(height~weight, data=women) #crio um modelo de regressao linear
+modeloW
+plot(modeloW)
+plot(height~weight, data=women)
+abline(modeloW)
+
+predict(modeloW, data.frame(weight=30)) #faz a previsao pro peso 30
