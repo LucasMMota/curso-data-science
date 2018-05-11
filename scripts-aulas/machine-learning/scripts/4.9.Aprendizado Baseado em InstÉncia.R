60f747cc20c49041f69d11b5957d3a7e97615110
Lucas Mendes <lucas.lmmf@gmail.com>
2018-05-11 15:44:56 -0300

    classificacao knn

--- scripts-aulas/machine-learning/scripts/4.9.Aprendizado Baseado em InstÉncia.R
+++ scripts-aulas/machine-learning/scripts/4.9.Aprendizado Baseado em InstÉncia.R
@@ -5,6 +5,7 @@ library(class)
 
 head(iris)
 summary(iris)
+dim(iris)
 
 amostra = sample(2,150,replace=T, prob=c(0.7,0.3))
 iristreino = iris[amostra==1,]
@@ -13,10 +14,9 @@ classificar = iris[amostra==2,]
 dim(iristreino)
 dim(classificar)
 
+#                 nao usa a classe [1:4]        classe na col 5   3 centroids
 previsao = knn(iristreino[,1:4],classificar[,1:4],iristreino[,5],k=3)
+table = table(classificar[,5],previsao) ## classificacao pelo vizinho mais proximo
 
-table(classificar[,5],previsao)
-
-(tabela[1] + tabela[5] + tabela[9]) / sum(tabela) 
-
+(table[1] + table[5] + table[9]) / sum(table) 
 
